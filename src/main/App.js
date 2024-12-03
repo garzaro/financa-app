ce24688a1c4f228aabe02586e6962b96078b6554
garzaro <clebergarzaro74@gmail.com>
2024-12-03 20:00:27 -0300

    commit incial

---
+++ src/main/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Login from "../views/login";
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+function App() {
+
+  return (
+
+    <div>
+
+      <Login />
+
+    </div>
+  )
+}
+
+export default App;
+
+/*APP RENDERIZA COMPONENTES*/
---
+++ src/views/login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../components/card";
+
+function Login() {
+
+    return (
+        
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6" style={{position: 'relative', left: '300px'}}>
+                    <div class="bs-docs-section">
+                        
+                        <Card title="Login">
+
+                        </Card>
+                        
+                        e ai truta, vai ou nao vai?
+                    </div>                
+                </div>
+            </div>
+        </div>         
+
+    )
+}
+
+export default Login;
