36323271fece5cd6d9289790b605037a6a0939a6
garzaro <clebergarzaro74@gmail.com>
2025-02-28 20:07:36 -0300

    aproximação dos campos do cadastro

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+import '../formulario.css'
 function FormGroup(props) {
 
     return (
         <div className="form-group text-success">
             <label htmlFor={props.htmlFor}>{props.label}</label>
             {props.children}
-
+            <br/>
         </div>
     );
 }
--- src/main/App.js
+++ src/main/App.js
@@ -3,13 +3,14 @@ import Login from "../views/login";
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
+
 import CadastroDeUsuario from "../views/cadastroDeUsuario";
 
 function App() {
   return (
     <div>
 
-      <CadastroDeUsuario/>
+      <Login />
 
     </div>
   );
