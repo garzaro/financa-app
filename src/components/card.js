7921c086c6b7a3dc0a574def53950bfa8796fa74
garzaro <clebergarzaro74@gmail.com>
2025-03-02 13:20:46 -0300

    alterado Card para function

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-class Card extends React.Component{
-
-    render() {
+function Card() {
         return (
-
             <div className="card mb-3 mt-4 text-success">
-                <h3 className="card-header">{this.props.title}</h3>
-                <div className="card-body ">
-                    {this.props.children}
-                </div>
+                    <h3 className="card-header">{this.props.title}</h3>
+                    <div className="card-body ">
+                            {this.props.children}
+                    </div>
             </div>
-        );
-    }
+        )
 }
-
 export default Card;
+
+
