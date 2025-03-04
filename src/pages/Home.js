d3cedd5e31e118042532e855debde1595a58c9e8
garzaro <clebergarzaro74@gmail.com>
2025-03-04 11:55:38 -0300

    adicionando Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+/*pagina inicial*/
+const Home = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Bem-vindo à Página Inicial</h1>
+        </div>
+    );
+};
+
+export default Home;
