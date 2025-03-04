3965cf44826bc13663a944fdcc49a46da82fcb27
garzaro <clebergarzaro74@gmail.com>
2025-03-04 11:53:52 -0300

    adicionando Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,15 @@
 import React from 'react';
-/*encapsula o fomulario*/
-const Card = ({ children }) => {
+/*encapsula o formulario de login*/
+function Card({title, children}) {
     return (
-        <div className="card">
-            <div className="card-body">
+        <div className="card mb-3 mt-4 text-success">
+            <h3 className="card-header">{title}</h3>
+            <div className="card-body ">
                 {children}
             </div>
         </div>
-    );
-};
+    )
+}
 export default Card;
+
+
