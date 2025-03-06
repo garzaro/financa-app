5a71958cf3bb304aa3856e1936d8162808053c1d
garzaro <clebergarzaro74@gmail.com>
2025-03-06 16:57:11 -0300

    adicionando useNavigate, navegar entre componentes

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,32 +1,106 @@
 import React from 'react';
 import Card from '../components/Card';
-/*cadastro de usuarios*/
-const Register = () => {
+import FormGroup from "../components/form-group";
+import {useNavigate} from "react-router-dom";
+
+import styled from "styled-components";
+
+/*pagina de cadastro de usuarios*/
+function Register () {
+    const navigate = useNavigate();
+
+    function handleCancelar() {
+        navigate('/Login');
+    }
+
+    const AsteriscoVermelho = styled.span`
+        color: red;
+    `;
     return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <Card>
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="name">Nome</label>
-                                <input type="text" className="form-control" id="name" placeholder="Digite seu nome" />
+        <div className="container-fluid mt-5 style={{minHeight: '0vh', display: 'flex', alignItems: 'center'">
+            <div className="row justify-content-center w-100">
+                <div className="col-md-6" style={{ marginTop: '-80px' }}> {/*style={{ marginTop: '-120px' }}*/}
+                    <div className="bs-docs-section">
+                        <Card title="Cadastro de Usuário">
+                            <div className="row justify-content-center align-items-center">
+                                <div className="col-md-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label={
+                                                <span>
+                                                Nome Completo: <AsteriscoVermelho>*</AsteriscoVermelho>
+                                            </span>
+                                            } name="nome-completo">
+                                                <input type="text"
+                                                       className="form-control inputPlaceholder"
+                                                       id="nome-completo"
+                                                       placeholder="Digite seu nome"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup label={
+                                                <span>
+                                                Cadastro Pessoa Física: <AsteriscoVermelho>*</AsteriscoVermelho>
+                                            </span>
+                                            } name="cpf">
+                                                <input type="text"
+                                                       className="form-control inputPlaceholder"
+                                                       id="cpf"
+                                                       placeholder="Digite seu nome cpf"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup label={
+                                                <span>
+                                                Nome de Usuário: <AsteriscoVermelho>*</AsteriscoVermelho>
+                                            </span>
+                                            } name="nome-usuario">
+                                                <input type="text"
+                                                       className="form-control inputPlaceholder"
+                                                       id="nome-usuario"
+                                                       placeholder="Nome de usuário"
+                                                />
+                                            </FormGroup>
+                                            <FormGroup label={
+                                                <span>
+                                                Email: <AsteriscoVermelho>*</AsteriscoVermelho>
+                                            </span>
+                                            } name="email">
+                                                <input type="email"
+                                                       className="form-control inputPlaceholder"
+                                                       id="email"
+                                                       placeholder="Digite o email"
+                                                />
+                                            </FormGroup>
+                                            {/*repetir*/}
+                                            <FormGroup label={
+                                                <span>
+                                                Confirmar Email: <AsteriscoVermelho>*</AsteriscoVermelho>
+                                            </span>
+                                            } name="confirmar-email">
+                                                <input type="email"
+                                                       className="form-control inputPlaceholder"
+                                                       id="repetir-email"
+                                                       placeholder="Confirme o email"
+                                                />
+                                            </FormGroup>
+                                            <button type="submit" className="btn btn-success mt-3">Confirmar</button>
+                                            <button type="button" className="btn btn-danger mt-3" onClick={handleCancelar}>Cancelar</button>
+                                        </fieldset>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" id="email" placeholder="Digite seu email" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Senha</label>
-                                <input type="password" className="form-control" id="password" placeholder="Digite sua senha" />
-                            </div>
-                            <button type="submit" className="btn btn-primary mt-3">Cadastrar</button>
-                        </form>
-                    </Card>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
-
 export default Register;
+
+
+
+
+
+
+
+
