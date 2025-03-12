8e074b0225d2b30614dcc1a14d126e832cd5779c
garzaro <clebergarzaro74@gmail.com>
2025-03-11 22:21:37 -0300

    ajustando maring botton, pra cima

--- src/pages/register.js
+++ src/pages/register.js
@@ -19,57 +19,61 @@ function Register () {
     return (
         <div className="container-fluid mt-5 style={{minHeight: '0vh', display: 'flex', alignItems: 'center'">
             <div className="row justify-content-center w-100">
-                <div className="col-md-6" style={{ marginTop: '-80px' }}> {/*style={{ marginTop: '-120px' }}*/}
+                <div className="col-md-6" style={{ marginTop: '-30px' }}> {/*style={{ marginTop: '-120px' }}*/}
                     <div className="bs-docs-section">
                         <Card title="Cadastro de Usuário">
                             <div className="row justify-content-center align-items-center">
-                                <div className="col-md-12">
+                                <div className="col-md-10">
                                     <div className="bs-component">
-                                        <fieldset>
+                                        <fieldset className="fieldset-sm">
                                             <FormGroup label={
                                                 <span>
                                                 Nome Completo: <AsteriscoVermelho>*</AsteriscoVermelho>
-                                            </span>
+                                                </span>
                                             } name="nome-completo">
                                                 <input type="text"
-                                                       className="form-control inputPlaceholder"
+                                                       className="form-control form-control-sm inputPlaceholder"
                                                        id="nome-completo"
                                                        placeholder="Digite seu nome"
                                                 />
                                             </FormGroup>
+
                                             <FormGroup label={
                                                 <span>
                                                 Cadastro Pessoa Física: <AsteriscoVermelho>*</AsteriscoVermelho>
                                             </span>
                                             } name="cpf">
                                                 <input type="text"
-                                                       className="form-control inputPlaceholder"
+                                                       className="form-control form-control-sm inputPlaceholder"
                                                        id="cpf"
                                                        placeholder="Digite seu nome cpf"
                                                 />
                                             </FormGroup>
+
                                             <FormGroup label={
                                                 <span>
                                                 Nome de Usuário: <AsteriscoVermelho>*</AsteriscoVermelho>
                                             </span>
                                             } name="nome-usuario">
                                                 <input type="text"
-                                                       className="form-control inputPlaceholder"
+                                                       className="form-control form-control-sm inputPlaceholder"
                                                        id="nome-usuario"
                                                        placeholder="Nome de usuário"
                                                 />
                                             </FormGroup>
+
                                             <FormGroup label={
                                                 <span>
                                                 Email: <AsteriscoVermelho>*</AsteriscoVermelho>
                                             </span>
                                             } name="email">
                                                 <input type="email"
-                                                       className="form-control inputPlaceholder"
+                                                       className="form-control form-control-sm inputPlaceholder"
                                                        id="email"
                                                        placeholder="Digite o email"
                                                 />
                                             </FormGroup>
+
                                             {/*repetir*/}
                                             <FormGroup label={
                                                 <span>
@@ -77,12 +81,13 @@ function Register () {
                                             </span>
                                             } name="confirmar-email">
                                                 <input type="email"
-                                                       className="form-control inputPlaceholder"
+                                                       className="form-control form-control-sm inputPlaceholder"
                                                        id="repetir-email"
                                                        placeholder="Confirme o email"
                                                 />
                                             </FormGroup>
                                             <button type="submit" className="btn btn-success mt-3">Confirmar</button>
+                                            &nbsp;&nbsp;
                                             <button type="button" className="btn btn-danger mt-3" onClick={handleCancelar}>Cancelar</button>
                                         </fieldset>
                                     </div>
