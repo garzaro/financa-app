186fee9328c1122b6194db980cf97aef406fcddd
garzaro <clebergarzaro74@gmail.com>
2025-03-11 22:22:21 -0300

    ajustando marging top, pra cima

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 {/*addBreak = true* opcional = <br>*/}
 function FormGroup({htmlFor, label, children, addBreak = false}){
     return (
-        <div className="form-group text-success" style={{ marginTop: '-15px' }}>
+        <div className="form-group text-success" style={{ marginTop: '5px' }}>
             <label htmlFor={htmlFor} className="text-success">{label}</label>
             {children}
             {addBreak && <br/>}
