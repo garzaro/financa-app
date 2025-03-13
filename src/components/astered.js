f5d5a8e1127ee28a61d106978fc3cc77bcd3dbdf
garzaro <clebergarzaro74@gmail.com>
2025-03-12 23:09:19 -0300

    adicionando asterisco vermelho

---
+++ src/components/astered.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/*asterisco vermelho no formulario*/
+const Astered = styled.span`
+    color: red;
+`;
+
+export default Astered;
