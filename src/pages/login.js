dce1ab0975e0b57cc4da96699ca2ac948575179d
garzaro <clebergarzaro74@gmail.com>
2025-03-23 23:45:35 -0300

    a continuar

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,21 +21,29 @@ const Login = () => {
     const fazerLogin = () =>{
         axios.post('http://localhost:8080/api/usuarios/autenticar', {
             email,
-            senha
+            senha,
         }).then(response => {
             setTimeout(() => {
                 navigate('/home');
             },2000)
         }).catch(err => {
-            setMensagensDeAlerta(err.response.data);
+            if (err.response && err.response.data) {
+                setMensagensDeAlerta(err.response.data);
+            }else{
+                setMensagensDeAlerta({geral: "ocorreu um erro inesperado"});
+            }
         });
     };
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
+        /*limpar o erro ao digitar*/
+        setMensagensDeAlerta('')
     }
     const handleSenhaChange = (e) => {
         setSenha(e.target.value);
+        /*limpar o erro ao digitar*/
+        setMensagensDeAlerta('')
     }
     /*para evitar que a pagina recarregue*/
     const handleSubmit = (e) => {
@@ -62,7 +70,7 @@ const Login = () => {
                         <ErrosLoginFront setMensagensDeAlerta={mensagensDeAlerta}></ErrosLoginFront>
                         */}
 
-                        <span>{mensagensDeAlerta}</span>
+                        {/* USAR ESSE AQUI NO REGISTER <span>{mensagensDeAlerta}</span>*/}
 
                         <Card title="Login">
 
@@ -76,39 +84,42 @@ const Login = () => {
                                                         Email:<span className="asterisco-vermelho">*</span>
                                                     </span>
                                                 } name="email"
-                                                           mensagemDeErro={setMensagensDeAlerta.email}
+                                                           mensagemDeErro={setMensagensDeAlerta}
                                                 >
                                                     <input
                                                         type="email"
                                                         value={email}
                                                         onChange={handleEmailChange}
-                                                        className="form-control form-control-sm inputPlaceholder"
+                                                        className={`form-control form-control-sm inputPlaceholder ${mensagensDeAlerta && 'is-invalid'}`}
                                                         id="email"
                                                         placeholder="Digite o email"
+                                                        required
                                                     />
+                                                    {mensagensDeAlerta && <div className="invalid-feedback">{mensagensDeAlerta}</div>}
                                                 </FormGroup>
+
                                                 <FormGroup label={
                                                     <span>
                                                         Senha:<span className="asterisco-vermelho">*</span>
                                                     </span>
                                                 } name="senha"
-                                                           mensagemDeErro={setMensagensDeAlerta.senha}
+                                                           mensagemDeErro={setMensagensDeAlerta}
                                                 >
                                                     <input type="password"
                                                            value={senha}
                                                            onChange={handleSenhaChange}
-                                                           className="form-control form-control-sm inputPlaceholder"
+                                                           className={`form-control form-control-sm inputPlaceholder ${mensagensDeAlerta && 'is-invalid'}`}
                                                            id="senha"
                                                            placeholder="Digite a senha"
+                                                           required
                                                     />
+                                                    {mensagensDeAlerta && <div className="invalid-feedback">{mensagensDeAlerta}</div>}
                                                 </FormGroup>
                                             </form>
                                             <button type="submit" onClick={fazerLogin} className="btn btn-success mt-3">Entrar</button>
                                             &nbsp;&nbsp;
                                             {/*type="button para evitar submissão do formulario"*/}
                                             <button type="button" className="btn btn-danger mt-3" onClick={handleCadastrar}>Cadastrar</button>
-
-
                                     </div>
                                 </div>
                             </div>
