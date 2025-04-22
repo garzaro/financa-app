de620b5e3c7e1351b04f069b33a49fcb1e3a1603
garzaro <clebergarzaro74@gmail.com>
2025-04-22 16:37:15 -0300

    ajustes

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,9 +6,7 @@ import Card from "../components/card";
 import FormGroup from "../components/form-group";
 /*ver esse questao dos campos controlados  e react hook form - nao pode usar os dois juntos -
 * ver essa expçicacao https://chatgpt.com/c/67f695c9-c94c-8013-851b-33a2dbdcd8ff */
-
 const LoginForm = () => {
-
     const {
         register,
         handleSubmit,
@@ -102,10 +100,14 @@ const LoginForm = () => {
                                             </div>
 
                                             {/* Botão de Login */}
+                                            <button type="submit" className="btn btn-success btn-sm mt-3">
+                                                Entrar
+                                            </button>
+                                            {/*
                                             <button type="submit" disabled={isLoading}
                                                     className="btn btn-success btn-sm mt-3 ">
                                                 {isLoading ? "Carregando..." : "Entrar"}
-                                            </button>
+                                            </button>*/}
 
                                         </form>
                                     </div>
