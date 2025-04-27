ca9d06a597402af0ee1815361722f53e239be3c9
garzaro <clebergarzaro74@gmail.com>
2025-04-27 10:25:07 -0300

    separando a logica do axios em servico

--- src/app/service/usuarioService.js
+++ src/app/service/usuarioService.js
@@ -2,15 +2,17 @@ import Apiservice from "../apiservice";
 /*tratando da api de serviços do usuario - sobrescrevendo para utilização dos metodos da apiService*/
 /*instanciando ApiService - composiçao*/
 
-const usuarioApi = Apiservice('/api/usuarios');
+const usuarioApi = Apiservice('/api/usuarios/');
 
-const UsuarioService = {
-    autenticar: (credenciais) => {
-        return Apiservice.post('/autenticar', credenciais);
-    },
+const ServiceUsuario = () =>{
+    return{
+        autenticar: (credenciais) => {
+        return usuarioApi.post('/autenticar', credenciais);
+    }
+};
     // outros métodos futuros:
     // cadastrar: (dados) => usuarioApi.post('/cadastrar', dados),
     // buscarPorId: (id) => usuarioApi.get(`/${id}`),
 };
 
-export default UsuarioService;
+export default ServiceUsuario;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,131 +1,127 @@
-import { useForm } from "react-hook-form";
-import axios from "axios";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import Card from "../components/card";
 import FormGroup from "../components/form-group";
 import {mensagemDeErro} from '../components/toastr'
+import UsuarioService from "../app/service/usuarioService";
 
-const LoginForm = () => {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    /*lib*/
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
-    const navigate = useNavigate();
+function LoginForm () {
+const [email, setEmail] = useState('');
+const [senha, setSenha] = useState('');
+const { register, handleSubmit, formState: { errors }} = useForm();
+const navigate = useNavigate();
+/*chamando o servico de usuario*/
+const usuarioService = UsuarioService();
+/*logar*/
+const fazerLogin = (data) => {
+usuarioService.autenticar({
+    email:data.email,
+    senha:data.senha,
+}).then(response => {
+/*recuperar o usuario da resposta - response*/
+    localStorage.setItem('_usuario_logado', JSON.stringify(response.data))
+    setTimeout(() => navigate("/home"), 2000);
+}).catch(err => {
+    mensagemDeErro(err.response.data);
+});
+};
+return (
+    <div className="container-fluid mt-5 style={{minHeight: '0vh', display: 'flex', flexDirection: 'column', alignItens:'center'}}>}}" >
+        <div className="row justify-content-center w-100" >
+            <div className="col-md-6">
+                <div className="bs-docs-section">
 
-    const fazerLogin = (data) => {
-        axios.post("http://localhost:8080/api/usuarios/autenticar", {
-            email:data.email,
-            senha:data.senha,
-        }).then(response => {
-            /*recuperar o usuario da resposta - response*/
-            localStorage.setItem('_usuario_logado', JSON.stringify(response.data))
-            setTimeout(() => navigate("/home"), 2000);
-        }).catch(err => {
-            mensagemDeErro(err.response.data);
-        });
-    };
+                    {/* Erros do Backend
+                    <div className="row">
+                        <span>{error}</span>
+                    </div>*/}
 
-
-    return (
-        <div className="container-fluid mt-5 style={{minHeight: '0vh', display: 'flex', flexDirection: 'column', alignItens:'center'}}>}}" >
-            <div className="row justify-content-center w-100" >
-                <div className="col-md-6">
-                    <div className="bs-docs-section">
-
-                        {/* Erros do Backend
+                    <Card title="Login">
                         <div className="row">
-                            <span>{error}</span>
-                        </div>*/}
+                            <div className="col-lg-12">
+                                <div className="bs-component">
+                                    <form onSubmit={handleSubmit(fazerLogin)}>
 
-                        <Card title="Login">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
-                                        <form onSubmit={handleSubmit(fazerLogin)}>
+                                        <FormGroup label={
+                                            <span>
+                                                Email:<span className="asterisco-vermelho">*</span>
+                                            </span>
+                                        } name={"email"}
+                                        >
+                                            {/* Campo E-mail */}
+                                            <input
+                                                type="email"
+                                                {...register("email", {required: "E-mail é obrigatório"})}
+                                                className="form-control form-control-sm inputPlaceholder"
+                                                placeholder="Digite seu email"
+                                                id="email"
+                                            />
+                                            {errors.email && <span className="error">{errors.email.message}</span>}
+                                        </FormGroup>
 
-                                            <FormGroup label={
-                                                <span>
-                                                    Email:<span className="asterisco-vermelho">*</span>
-                                                </span>
-                                            } name={"email"}
-                                            >
-                                                {/* Campo E-mail */}
-                                                <input
-                                                    type="email"
-                                                    {...register("email", {required: "E-mail é obrigatório"})}
-                                                    className="form-control form-control-sm inputPlaceholder"
-                                                    placeholder="Digite seu email"
-                                                    id="email"
-                                                />
-                                                {errors.email && <span className="error">{errors.email.message}</span>}
-                                            </FormGroup>
+                                        <FormGroup label={
+                                            <span>
+                                                Senha:<span className="asterisco-vermelho">*</span>
+                                            </span>
+                                        } name={"senha"}
+                                        >
+                                            {/* Campo Senha */}
+                                            <input
+                                                type="password"
+                                                {...register("senha", {required: "Senha é obrigatória"})}
+                                                className="form-control form-control-sm inputPlaceholder"
+                                                placeholder="Digite sua senha"
+                                            />
+                                            {errors.senha && <span className="error">{errors.senha.message}</span>}
+                                        </FormGroup>
 
-                                            <FormGroup label={
-                                                <span>
-                                                    Senha:<span className="asterisco-vermelho">*</span>
-                                                </span>
-                                            } name={"senha"}
-                                            >
-                                                {/* Campo Senha */}
-                                                <input
-                                                    type="password"
-                                                    {...register("senha", {required: "Senha é obrigatória"})}
-                                                    className="form-control form-control-sm inputPlaceholder"
-                                                    placeholder="Digite sua senha"
-                                                />
-                                                {errors.senha && <span className="error">{errors.senha.message}</span>}
-                                            </FormGroup>
+                                        {/*esqueceu a senha*/}
+                                        <div className="nav-signin-tooltip-footer ">Esqueceu a senha?
+                                            <a href="/register"
+                                               className="nav-a"
+                                               aria-label="Esqueceu a senha? Clique aqui para criar uma nova.">&nbsp;
+                                                Clique aqui.</a>
+                                        </div>
 
-                                            {/*esqueceu a senha*/}
-                                            <div className="nav-signin-tooltip-footer ">Esqueceu a senha?
-                                                <a href="/register"
-                                                   className="nav-a"
-                                                   aria-label="Esqueceu a senha? Clique aqui para criar uma nova.">&nbsp;
-                                                    Clique aqui.</a>
-                                            </div>
+                                        {/* Botão de Login */}
+                                        <button type="submit"
+                                                className="btn btn-success btn-sm mt-3 ">Entrar
+                                        </button>
 
-                                            {/* Botão de Login */}
-                                            <button type="submit"
-                                                    className="btn btn-success btn-sm mt-3 ">Entrar
-                                            </button>
-
-                                        </form>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
-                        </Card>
-                        {/*cadastro*/}
-                        <Card title="Seja bem vindo!">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
-                                        <div className="card-body">
-                                            <div className="m-sm-4">
-                                                <h2 className="text-center">Primeiro acesso?</h2>
-                                                <p className="text-center mb-3">
-                                                    Se ainda não possui acesso, clique no
-                                                    botão abaixo, crie sua conta e obtenha acesso ao Financas Pessoais.</p>
-                                                <div className="text-center">
-                                                    <a href="/register" className="btn btn-sm btn-warning"
-                                                       title="Não tem uma conta? Crie sua conta!">Criar conta</a>
-                                                </div>
+                        </div>
+                    </Card>
+                    {/*cadastro*/}
+                    <Card title="Seja bem vindo!">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="bs-component">
+                                    <div className="card-body">
+                                        <div className="m-sm-4">
+                                            <h2 className="text-center">Primeiro acesso?</h2>
+                                            <p className="text-center mb-3">
+                                                Se ainda não possui acesso, clique no
+                                                botão abaixo, crie sua conta e obtenha acesso ao Financas Pessoais.</p>
+                                            <div className="text-center">
+                                                <a href="/register" className="btn btn-sm btn-warning"
+                                                   title="Não tem uma conta? Crie sua conta!">Criar conta</a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </Card>
+                        </div>
+                    </Card>
 
-                    </div>
                 </div>
             </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default LoginForm;
