a54c969ef2cf7e0426fc3b9f04d6d99863506763
garzaro <clebergarzaro74@gmail.com>
2025-05-27 20:09:26 -0300

    adicionando consultarLancamentos

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -32,8 +32,13 @@ const Navbar = () => {
                         </li>
 
                         <li className="nav-item">
+                            <Link className="nav-link" to="/consultar-lancamentos">Lançamentos</Link>
+                        </li>
+
+                        <li className="nav-item">
                             <Link className="nav-link" to="/login">Login</Link>
                         </li>
+
                         <li className="nav-item">
                             <Link className="nav-link" to="/cadastroUsuario">Cadastro</Link>
 
