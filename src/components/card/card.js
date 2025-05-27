af835cbe38b365b8890746e5173d2c9d5129fc0c
garzaro <clebergarzaro74@gmail.com>
2025-05-27 20:07:55 -0300

    ajsute

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 /*encapsula o formulario de login*/
 function Card({title, children}) {
     return (
-        <div className="card card-custom mb-5 text-white" style={{ marginTop: '-90px', backgroundColor: 'rgba(108,119,131,0.61)' }}>
+        <div className="card card-custom mb-5 text-white" style={{ marginTop: '-40.0px', backgroundColor: 'rgba(108,119,131,0.61)' }}>
             <h3 className="card-header text-center">{title}</h3>
             <div className="card-body ">
                 {children}
