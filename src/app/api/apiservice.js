7d7cb479a26c30736b86eeaf4c0019c60838b2a3
garzaro <clebergarzaro74@gmail.com>
2025-08-14 22:48:28 -0300

    ajustes

--- src/app/api/apiservice.js
+++ src/app/api/apiservice.js
@@ -8,7 +8,7 @@ import axios from "axios";
  * export const httpClient = axios.create({
  *     baseURL: 'http://localhost:8080/',
  *     withCredentials: true
- * })
+ * });
  * */
 
 console.log("API Base URL:", process.env.REACT_APP_API_BASE_URL);
