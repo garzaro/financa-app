8facaf8926b6a5878f19ce1f1d3f9270a0d6e8c1
garzaro <clebergarzaro74@gmail.com>
2025-09-03 20:00:34 -0300

    corrigindo erro se props sx no Backdrop

--- src/components/feedback/loader.js
+++ src/components/feedback/loader.js
@@ -8,12 +8,12 @@ export default function PanoDeFundo({open= false, color, titulo, mensagem,}){
     return (
         <div>
             <Backdrop
-                xs={(theme) => ({color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                sx={(theme) => ({color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
                 open={open}
             >
                 <div style={{ textAlign: "center" }}>
                     <CircularProgress color={color}/>
-                    <Typography variant="body2" color="white">
+                    <Typography variant="body2" color="white" component="div">
                         <h1>{titulo}</h1>
                         <p>{mensagem}</p>
                     </Typography>
