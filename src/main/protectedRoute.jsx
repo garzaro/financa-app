93aed732f1ab31f848662aaf91458b97c127ee7a
garzaro <clebergarzaro74@gmail.com>
2026-01-22 22:05:24 -0300

    Rota protegida

---
+++ src/main/protectedRoute.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navigate, Outlet} from 'react-router-dom';
+
+const isAuthenticated = () => {
+    return !!localStorage.getItem('_usuario_logado');
+}
+const ProtectedRoute = () => {
+  return isAuthenticated() ? <Outlet /> : <Navigate to='/login' replace />;
+}
+export default ProtectedRoute;
