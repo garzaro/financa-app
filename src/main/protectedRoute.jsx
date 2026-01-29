702dc8d69cbaac2b5a3a411c60b1b265e23a1fa4
garzaro <clebergarzaro74@gmail.com>
2026-01-28 23:26:56 -0300

    ajustes

--- src/main/protectedRoute.jsx
+++ src/main/protectedRoute.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Navigate, Outlet, Route, useLocation} from 'react-router-dom';
 import {LocalStorageService} from "../app/service/localStorageService.js";
-// import AuthService from "../app/service/authService.js";
 
 const storageLocal = LocalStorageService();
 
@@ -20,29 +19,3 @@ function ProtectedRoute () {
   return <Outlet location={location}/>
 }
 export default ProtectedRoute;
-
-
-
-
-
-/**
- * https://www.robinwieruch.de/react-router-private-routes/
- *
- * https://medium.com/@ravisharma23523/safeguard-your-react-app-implementing-secure-routing-with-react-router-2adf0299aaca
- ***/
-
-/**
- *
- * const isAuthenticated = () => {
- *   const usuario = storageLocal.obterItem('_usuario_logado')
- *    /** !! (double bang) para converter o objeto usuario em um valor booleano puro.**
- *     return !!usuario && Object.keys(usuario).length > 0;
- * }
- * const ProtectedRoute = () => {
- *   const location = useLocation(); /**escuta mudança de url**
- *    //return AuthService ? <Outlet /> : <Navigate to='/login' replace />;
- *  return isAuthenticated () ? <Outlet/> : <Navigate to='/login' state={{from: location}} replace/>;
- *}
- * export default ProtectedRoute;
- *
- * **/
