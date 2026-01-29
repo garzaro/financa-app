2b49f791a2b13f93abd8a0bb59321696461047cf
garzaro <clebergarzaro74@gmail.com>
2026-01-29 18:01:07 -0300

    ajustes py mt mb

--- src/components/template/card.jsx
+++ src/components/template/card.jsx
@@ -4,8 +4,8 @@ function Card({title, children}) {
   return (
     <div className="card w-100 text-white border-2 border-primary shadow-lg"
          style={{marginTop: '-60px', backgroundColor: 'rgb(30,29,29)' }}>
-      <h3 className="mt-0 text-center" style={{ fontSize: '18px', letterSpacing: '2px' }}>{ title }</h3>
-      <div className="p-2 py-0 mt-0 mb-2 " style={{ fontSize: '12px', letterSpacing: '2px' }}>
+      <h3 className="mt-2 text-center" style={{ fontSize: '18px', letterSpacing: '2px' }}>{ title }</h3>
+      <div className="p-4 py-2 mt-2 mb-2 " style={{ fontSize: '12px', letterSpacing: '2px' }}>
         {children}
       </div>
     </div>
