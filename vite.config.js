3a0b85a90145c2d97ab4bca369254d8e3cb020d3
garzaro <clebergarzaro74@gmail.com>
2026-02-05 22:54:25 -0300

    atualizacaoes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   server: {
     port: 3000,
     open: true,
