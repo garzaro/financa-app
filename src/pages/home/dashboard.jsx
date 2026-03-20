bcc3cca9b16514a54f0f35b7cac85ecb3827b017
garzaro <clebergarzaro74@gmail.com>
2026-03-20 00:42:00 -0300

    iniciando o dashboard

---
+++ src/pages/home/dashboard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+/**
+ * to-do list
+ * [] Trazer saldo
+ * [] Trazer outros servicos para dashboard - buscar outras informações
+ *
+ * **/
+export default function Dashboard () {
+  /**
+   * @param saldoServico é a composicao - em componente de classe
+   * seria feito um extends do servico e depois o construtor para
+   * instanciar o servico.
+   * @param service = new UsuarioService();
+   * */
+    //const { saldo, loading, erro } = useSaldo();
+  const { loggedUser } = useAuth();
+  const saldoServico = UsuarioService();
+  const [saldo, setSaldo] = useState(0);
+  const [saldoPendente, setSaldoPendente] = useState(0);
+
+
+  /**
+   * retornar o saldo do usuario logado
+   * */
+  useEffect(() => {
+    const buscarSaldo = () => {
+      if (!loggedUser || !loggedUser.id) return;
+
+      console.log('Usuario recuperado do contexto', loggedUser);
+      saldoServico.buscarSaldoPorUsuario(loggedUser.id)
+        .then(respondeAiManoBanco => {
+          setSaldo(respondeAiManoBanco.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+    buscarSaldo();
+  }, [loggedUser, saldoServico]);
+
+  function capitalizar(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  return (
+    <h1>Painel do usuário</h1>
+  )
+}
